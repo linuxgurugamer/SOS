5167755169ba81482db60c9998dbbe8a9528c45d
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-10-02 23:24:44 -0400

    Added AssemblyFileVersion Updated version file for 1.12

--- deploy.bat
+++ deploy.bat
@@ -8,12 +8,16 @@ rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
 rem    but not always
 
 set H=%KSPDIR%
-set H=R:\KSP_1.9.1_dev
 set GAMEDIR=SOS
 set GAMEDATA="GameData"
 set VERSIONFILE=%GAMEDIR%.version
 
+set DP0=r:\dp0\kspdev
+
 copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%\Plugins"
+copy /Y "%1%3".pdb "%GAMEDATA%\%GAMEDIR%\Plugins"
+
 copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
 
 xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
+xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%DP0%\GameData\%GAMEDIR%"
